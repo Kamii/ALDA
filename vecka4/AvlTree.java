dd17a12d26ba6131ac2a9136c79dbe8cc18f7295
Kamyar Sajjadi <kamyar_sajjadi@gmail.com>
2012-01-24 16:40:45 +0100

    Added AvlTree with the basicmethods

--- vecka4/AvlTree.java
+++ vecka4/AvlTree.java
@@ -21,303 +21,420 @@
  */
 public class AvlTree<AnyType extends Comparable<? super AnyType>>
 {
-    /**
-     * Construct the tree.
-     */
-    public AvlTree( )
-    {
-        root = null;
-    }
+	/**
+	 * Construct the tree.
+	 */
+	public AvlTree( )
+	{
+		root = null;
+	}
 
-    /**
-     * Insert into the tree; duplicates are ignored.
-     * @param x the item to insert.
-     */
-    public void insert( AnyType x )
-    {
-        root = insert( x, root );
-    }
+	/**
+	 * Insert into the tree; duplicates are ignored.
+	 * @param x the item to insert.
+	 */
+	public void insert( AnyType x )
+	{
+		root = insert( x, root );
+	}
 
-    /**
-     * Remove from the tree. Nothing is done if x is not found.
-     * @param x the item to remove.
-     */
-    public void remove( AnyType x )
-    {
-        System.out.println( "Sorry, remove unimplemented" );
-    }
+	/**
+	 * Remove from the tree. Nothing is done if x is not found.
+	 * @param x the item to remove.
+	 */
+	public void remove( AnyType x )
+	{
+		System.out.println( "Sorry, remove unimplemented" );
+	}
 
-    /**
-     * Find the smallest item in the tree.
-     * @return smallest item or null if empty.
-     */
-    public AnyType findMin( )
-    {
-        if( isEmpty( ) )
-            throw new UnderflowException( );
-        return findMin( root ).element;
-    }
+	/**
+	 * Find the smallest item in the tree.
+	 * @return smallest item or null if empty.
+	 */
+	public AnyType findMin( )
+	{
+		if( isEmpty( ) )
+			throw new UnderflowException( );
+		return findMin( root ).element;
+	}
 
-    /**
-     * Find the largest item in the tree.
-     * @return the largest item of null if empty.
-     */
-    public AnyType findMax( )
-    {
-        if( isEmpty( ) )
-            throw new UnderflowException( );
-        return findMax( root ).element;
-    }
+	/**
+	 * Find the largest item in the tree.
+	 * @return the largest item of null if empty.
+	 */
+	public AnyType findMax( )
+	{
+		if( isEmpty( ) )
+			throw new UnderflowException( );
+		return findMax( root ).element;
+	}
 
-    /**
-     * Find an item in the tree.
-     * @param x the item to search for.
-     * @return true if x is found.
-     */
-    public boolean contains( AnyType x )
-    {
-        return contains( x, root );
-    }
+	/**
+	 * Find an item in the tree.
+	 * @param x the item to search for.
+	 * @return true if x is found.
+	 */
+	public boolean contains( AnyType x )
+	{
+		return contains( x, root );
+	}
 
-    /**
-     * Make the tree logically empty.
-     */
-    public void makeEmpty( )
-    {
-        root = null;
-    }
+	/**
+	 * Make the tree logically empty.
+	 */
+	public void makeEmpty( )
+	{
+		root = null;
+	}
 
-    /**
-     * Test if the tree is logically empty.
-     * @return true if empty, false otherwise.
-     */
-    public boolean isEmpty( )
-    {
-        return root == null;
-    }
+	/**
+	 * Test if the tree is logically empty.
+	 * @return true if empty, false otherwise.
+	 */
+	public boolean isEmpty( )
+	{
+		return root == null;
+	}
 
-    /**
-     * Print the tree contents in sorted order.
-     */
-    public void printTree( )
-    {
-        if( isEmpty( ) )
-            System.out.println( "Empty tree" );
-        else
-            printTree( root );
-    }
-    
-    /**
-     * Internal method to insert into a subtree.
-     * @param x the item to insert.
-     * @param t the node that roots the subtree.
-     * @return the new root of the subtree.
-     */
-    private AvlNode<AnyType> insert( AnyType x, AvlNode<AnyType> t )
-    {
-        if( t == null )
-            return new AvlNode<AnyType>( x, null, null );
-        
-        int compareResult = x.compareTo( t.element );
-        
-        if( compareResult < 0 )
-        {
-            t.left = insert( x, t.left );
-            if( height( t.left ) - height( t.right ) == 2 )
-                if( x.compareTo( t.left.element ) < 0 )
-                    t = rotateWithLeftChild( t );
-                else
-                    t = doubleWithLeftChild( t );
-        }
-        else if( compareResult > 0 )
-        {
-            t.right = insert( x, t.right );
-            if( height( t.right ) - height( t.left ) == 2 )
-                if( x.compareTo( t.right.element ) > 0 )
-                    t = rotateWithRightChild( t );
-                else
-                    t = doubleWithRightChild( t );
-        }
-        else
-            ;  // Duplicate; do nothing
-        t.height = Math.max( height( t.left ), height( t.right ) ) + 1;
-        return t;
-    }
+	/**
+	 * Print the tree contents in sorted order.
+	 */
+	public void printTree( )
+	{
+		if( isEmpty( ) )
+			System.out.println( "Empty tree" );
+		else
+			printTree( root );
+	}
 
-    /**
-     * Internal method to find the smallest item in a subtree.
-     * @param t the node that roots the tree.
-     * @return node containing the smallest item.
-     */
-    private AvlNode<AnyType> findMin( AvlNode<AnyType> t )
-    {
-        if( t == null )
-            return t;
+	/**
+	 * Return size of the tree.
+	 */
+	public int size()
+	{
+		return size(root);
+	}
 
-        while( t.left != null )
-            t = t.left;
-        return t;
-    }
+	/**
+	 * Return the max height of the tree.
+	 */
+	public int maxHeight()
+	{
+		return maxHeight(root);
+	}
 
-    /**
-     * Internal method to find the largest item in a subtree.
-     * @param t the node that roots the tree.
-     * @return node containing the largest item.
-     */
-    private AvlNode<AnyType> findMax( AvlNode<AnyType> t )
-    {
-        if( t == null )
-            return t;
+	/**
+	 * See if the tree is a binary tree.
+	 */
+	public boolean isSearchTree()
+	{
+		return isSearchTree(root);
+	}
 
-        while( t.right != null )
-            t = t.right;
-        return t;
-    }
+	/**
+	 * Controll if the height of the tree is correct.
+	 */
+	public boolean hasCorrectHeightInfo()
+	{
+		return hasCorrectHeightInfo(root);
+	}
 
-    /**
-     * Internal method to find an item in a subtree.
-     * @param x is item to search for.
-     * @param t the node that roots the tree.
-     * @return true if x is found in subtree.
-     */
-    private boolean contains( AnyType x, AvlNode<AnyType> t )
-    {
-        while( t != null )
-        {
-            int compareResult = x.compareTo( t.element );
-            
-            if( compareResult < 0 )
-                t = t.left;
-            else if( compareResult > 0 )
-                t = t.right;
-            else
-                return true;    // Match
-        }
+	/**
+	 * Internal method to get the size.
+	 * @param node the node that roots the subtree.
+	 * @return the size of the tree (int).
+	 */
+	private int size(AvlNode<AnyType> node)
+	{
+		if(node == null)
+			return 0;
+		else
+			return size(node.left) + size(node.right) + 1;
+	}
 
-        return false;   // No match
-    }
+	/**
+	 * Internal method to get the max height of the tree.
+	 * @param node the node that roots the subtree.
+	 * @return the max height of the tree. 
+	 */
+	private int maxHeight(AvlNode<AnyType> node)
+	{
+		int height = 0;
+		
+		if(node == null)
+			return -1;
+		else
+			return Math.max(maxHeight(node.left)+1, maxHeight(node.right)+1);
+	}
 
-    /**
-     * Internal method to print a subtree in sorted order.
-     * @param t the node that roots the tree.
-     */
-    private void printTree( AvlNode<AnyType> t )
-    {
-        if( t != null )
-        {
-            printTree( t.left );
-            System.out.println( t.element );
-            printTree( t.right );
-        }
-    }
+	/**
+	 * Internal method to control if the tree is a binary tree. 
+	 * @param node the node that roots the subtree.
+	 * @return true if the tree is binary else false. 
+	 */
+	private boolean isSearchTree(AvlNode<AnyType> node)
+	{
+		boolean l, r;
 
-    /**
-     * Return the height of node t, or -1, if null.
-     */
-    private int height( AvlNode<AnyType> t )
-    {
-        return t == null ? -1 : t.height;
-    }
+		if(node.left == null)
+			l = true;
+		else
+		{
+			int left = node.element.compareTo(node.left.element);
+			if(left > 0)
+				l = isSearchTree(node.left);
+			else
+				l = false;
+		}
 
-    /**
-     * Rotate binary tree node with left child.
-     * For AVL trees, this is a single rotation for case 1.
-     * Update heights, then return new root.
-     */
-    private AvlNode<AnyType> rotateWithLeftChild( AvlNode<AnyType> k2 )
-    {
-        AvlNode<AnyType> k1 = k2.left;
-        k2.left = k1.right;
-        k1.right = k2;
-        k2.height = Math.max( height( k2.left ), height( k2.right ) ) + 1;
-        k1.height = Math.max( height( k1.left ), k2.height ) + 1;
-        return k1;
-    }
+		if(node.right == null)
+			r = true;
+		else
+		{
+			int right = node.element.compareTo(node.right.element);
+			if(right < 0)
+				r = isSearchTree(node.right);
+			else
+				r = false;
+		}
 
-    /**
-     * Rotate binary tree node with right child.
-     * For AVL trees, this is a single rotation for case 4.
-     * Update heights, then return new root.
-     */
-    private AvlNode<AnyType> rotateWithRightChild( AvlNode<AnyType> k1 )
-    {
-        AvlNode<AnyType> k2 = k1.right;
-        k1.right = k2.left;
-        k2.left = k1;
-        k1.height = Math.max( height( k1.left ), height( k1.right ) ) + 1;
-        k2.height = Math.max( height( k2.right ), k1.height ) + 1;
-        return k2;
-    }
+		return l && r;
+	}
 
-    /**
-     * Double rotate binary tree node: first left child
-     * with its right child; then node k3 with new left child.
-     * For AVL trees, this is a double rotation for case 2.
-     * Update heights, then return new root.
-     */
-    private AvlNode<AnyType> doubleWithLeftChild( AvlNode<AnyType> k3 )
-    {
-        k3.left = rotateWithRightChild( k3.left );
-        return rotateWithLeftChild( k3 );
-    }
+	/**
+	 * Internal method to insert into a subtree.
+	 * @param x the item to insert.
+	 * @param t the node that roots the subtree.
+	 * @return the new root of the subtree.
+	 */
+	private AvlNode<AnyType> insert( AnyType x, AvlNode<AnyType> t )
+	{
+		if( t == null )
+			return new AvlNode<AnyType>( x, null, null );
 
-    /**
-     * Double rotate binary tree node: first right child
-     * with its left child; then node k1 with new right child.
-     * For AVL trees, this is a double rotation for case 3.
-     * Update heights, then return new root.
-     */
-    private AvlNode<AnyType> doubleWithRightChild( AvlNode<AnyType> k1 )
-    {
-        k1.right = rotateWithLeftChild( k1.right );
-        return rotateWithRightChild( k1 );
-    }
+		int compareResult = x.compareTo( t.element );
 
-    private static class AvlNode<AnyType>
-    {
-            // Constructors
-        AvlNode( AnyType theElement )
-        {
-            this( theElement, null, null );
-        }
+		if( compareResult < 0 )
+		{
+			t.left = insert( x, t.left );
+			if( height( t.left ) - height( t.right ) == 2 )
+				if( x.compareTo( t.left.element ) < 0 )
+					t = rotateWithLeftChild( t );
+				else
+					t = doubleWithLeftChild( t );
+		}
+		else if( compareResult > 0 )
+		{
+			t.right = insert( x, t.right );
+			if( height( t.right ) - height( t.left ) == 2 )
+				if( x.compareTo( t.right.element ) > 0 )
+					t = rotateWithRightChild( t );
+				else
+					t = doubleWithRightChild( t );
+		}
+		else
+			;  // Duplicate; do nothing
+		t.height = Math.max( height( t.left ), height( t.right ) ) + 1;
+		return t;
+	}
 
-        AvlNode( AnyType theElement, AvlNode<AnyType> lt, AvlNode<AnyType> rt )
-        {
-            element  = theElement;
-            left     = lt;
-            right    = rt;
-            height   = 0;
-        }
+	/**
+	 * Internal method to find the smallest item in a subtree.
+	 * @param t the node that roots the tree.
+	 * @return node containing the smallest item.
+	 */
+	private AvlNode<AnyType> findMin( AvlNode<AnyType> t )
+	{
+		if( t == null )
+			return t;
 
-        AnyType           element;      // The data in the node
-        AvlNode<AnyType>  left;         // Left child
-        AvlNode<AnyType>  right;        // Right child
-        int               height;       // Height
-    }
+		while( t.left != null )
+			t = t.left;
+		return t;
+	}
 
-      /** The tree root. */
-    private AvlNode<AnyType> root;
+	/**
+	 * Internal method to find the largest item in a subtree.
+	 * @param t the node that roots the tree.
+	 * @return node containing the largest item.
+	 */
+	private AvlNode<AnyType> findMax( AvlNode<AnyType> t )
+	{
+		if( t == null )
+			return t;
+
+		while( t.right != null )
+			t = t.right;
+		return t;
+	}
+
+	/**
+	 * Internal method to find an item in a subtree.
+	 * @param x is item to search for.
+	 * @param t the node that roots the tree.
+	 * @return true if x is found in subtree.
+	 */
+	private boolean contains( AnyType x, AvlNode<AnyType> t )
+	{
+		while( t != null )
+		{
+			int compareResult = x.compareTo( t.element );
+
+			if( compareResult < 0 )
+				t = t.left;
+			else if( compareResult > 0 )
+				t = t.right;
+			else
+				return true;    // Match
+		}
+
+		return false;   // No match
+	}
+
+	/**
+	 * Internal method to print a subtree in sorted order.
+	 * @param t the node that roots the tree.
+	 */
+	private void printTree( AvlNode<AnyType> t )
+	{
+		if( t != null )
+		{
+			printTree( t.left );
+			System.out.println( t.element );
+			printTree( t.right );
+		}
+	}
+
+	/**
+	 * Return the height of node t, or -1, if null.
+	 */
+	private int height( AvlNode<AnyType> t )
+	{
+		return t == null ? -1 : t.height;
+	}
+
+	/**
+	 * Rotate binary tree node with left child.
+	 * For AVL trees, this is a single rotation for case 1.
+	 * Update heights, then return new root.
+	 */
+	private AvlNode<AnyType> rotateWithLeftChild( AvlNode<AnyType> k2 )
+	{
+		AvlNode<AnyType> k1 = k2.left;
+		k2.left = k1.right;
+		k1.right = k2;
+		k2.height = Math.max( height( k2.left ), height( k2.right ) ) + 1;
+		k1.height = Math.max( height( k1.left ), k2.height ) + 1;
+		return k1;
+	}
+
+	/**
+	 * Rotate binary tree node with right child.
+	 * For AVL trees, this is a single rotation for case 4.
+	 * Update heights, then return new root.
+	 */
+	private AvlNode<AnyType> rotateWithRightChild( AvlNode<AnyType> k1 )
+	{
+		AvlNode<AnyType> k2 = k1.right;
+		k1.right = k2.left;
+		k2.left = k1;
+		k1.height = Math.max( height( k1.left ), height( k1.right ) ) + 1;
+		k2.height = Math.max( height( k2.right ), k1.height ) + 1;
+		return k2;
+	}
+
+	/**
+	 * Double rotate binary tree node: first left child
+	 * with its right child; then node k3 with new left child.
+	 * For AVL trees, this is a double rotation for case 2.
+	 * Update heights, then return new root.
+	 */
+	private AvlNode<AnyType> doubleWithLeftChild( AvlNode<AnyType> k3 )
+	{
+		k3.left = rotateWithRightChild( k3.left );
+		return rotateWithLeftChild( k3 );
+	}
+
+	/**
+	 * Double rotate binary tree node: first right child
+	 * with its left child; then node k1 with new right child.
+	 * For AVL trees, this is a double rotation for case 3.
+	 * Update heights, then return new root.
+	 */
+	private AvlNode<AnyType> doubleWithRightChild( AvlNode<AnyType> k1 )
+	{
+		k1.right = rotateWithLeftChild( k1.right );
+		return rotateWithRightChild( k1 );
+	}
+
+	/**
+	 * Class for thorwing exception. 
+	 * No biggi
+	 */
+	private static class UnderflowException extends RuntimeException
+	{
+		public UnderflowException()
+		{
+		}
+
+		public UnderflowException(String str)
+		{
+			super(str);
+		}
+	}
+
+	private static class AvlNode<AnyType>
+	{
+		// Constructors
+		AvlNode( AnyType theElement )
+		{
+			this( theElement, null, null );
+		}
+
+		AvlNode( AnyType theElement, AvlNode<AnyType> lt, AvlNode<AnyType> rt )
+		{
+			element  = theElement;
+			left     = lt;
+			right    = rt;
+			height   = 0;
+		}
+
+		AnyType           element;      // The data in the node
+		AvlNode<AnyType>  left;         // Left child
+		AvlNode<AnyType>  right;        // Right child
+		int               height;       // Height
+	}
 
 
-        // Test program
-    public static void main( String [ ] args )
-    {
-        AvlTree<Integer> t = new AvlTree<Integer>( );
-        final int NUMS = 4000;
-        final int GAP  =   37;
+	/** The tree root. */
+	private AvlNode<AnyType> root;
 
-        System.out.println( "Checking... (no more output means success)" );
 
-        for( int i = GAP; i != 0; i = ( i + GAP ) % NUMS )
-            t.insert( i );
+	// Test program
+	public static void main( String [ ] args )
+	{
+		AvlTree<Integer> t = new AvlTree<Integer>( );
+		final int NUMS = 40;
+		final int GAP  =   37;
 
-        if( NUMS < 40 )
-            t.printTree( );
-        if( t.findMin( ) != 1 || t.findMax( ) != NUMS - 1 )
-            System.out.println( "FindMin or FindMax error!" );
+		System.out.println( "Checking... (no more output means success)" );
 
-        for( int i = 1; i < NUMS; i++ )
-            if( !t.contains( i ) )
-               System.out.println( "Find error1!" );
-    }
+		for( int i = GAP; i != 0; i = ( i + GAP ) % NUMS )
+			t.insert( i );
+
+		System.out.println("Size: " + t.size());
+		System.out.println("Max : " + t.maxHeight());
+		t.printTree( );
+		System.out.println(t.isSearchTree());
+		System.out.println("correct : " + t.hasCorrectHeightInfo());
+
+//		if( NUMS < 40 )
+//			t.printTree( );
+//		if( t.findMin( ) != 1 || t.findMax( ) != NUMS - 1 )
+//			System.out.println( "FindMin or FindMax error!" );
+//
+//		for( int i = 1; i < NUMS; i++ )
+//			if( !t.contains( i ) )
+//				System.out.println( "Find error1!" );
+	}
 }
