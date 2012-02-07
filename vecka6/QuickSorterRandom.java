cc10e8cea0b496e7697d4e2938167ec55ffc3804
Kamyar Sajjadi <kamyar_sajjadi@gmail.com>
2012-02-07 17:02:44 +0100

    käften erik

--- vecka6/QuickSorterRandom.java
+++ vecka6/QuickSorterRandom.java
@@ -1,26 +1,415 @@
-package alda.sort;
+// package alda.sort;
 
-import java.util.List;
+// import java.util.List;
+import java.util.*;
 
 /**
- * Detta är en av algoritmerna du ska implementera själv. Algoritmen ska vara
- * quicksort, och pivotvärdet ska väljas slumpmässigt, det som på
- * sidan 266 i boken förklaras vara "a safe maneuver".
+ * Detta Ã¤r en av algoritmerna du ska implementera sjÃ¤lv. Algoritmen ska vara
+ * quicksort, och pivotvÃ¤rdet ska vÃ¤ljas slumpmÃ¤ssigt, det som pÃ¥
+ * sidan 266 i boken fÃ¶rklaras vara "a safe maneuver".
  * 
- * För information om quicksort generellt: se kursboken sidan 264 och framåt.
- * Tänk dock på att implementationen i boken jobbar på arrayer, men den här
- * jobbar på en lista av godtycklig typ och att den använder en annan
- * pivotvalsstrategi vilket kräver att man skriver om den lite grann.
+ * FÃ¶r information om quicksort generellt: se kursboken sidan 264 och framÃ¥t.
+ * TÃ¤nk dock pÃ¥ att implementationen i boken jobbar pÃ¥ arrayer, men den hÃ¤r
+ * jobbar pÃ¥ en lista av godtycklig typ och att den anvÃ¤nder en annan
+ * pivotvalsstrategi vilket krÃ¤ver att man skriver om den lite grann.
  */
 public class QuickSorterRandom<T extends Comparable<? super T>> extends Sorter<T> {
 
-	@Override
-	protected void doSort(List<T> l) {
-		// TODO Auto-generated method stub
-		
+	private static final int CUTOFF = 10;
+
+	private void insertionSort(List<T> l, int left, int right) {
+		int j;
+		for (int p = left + 1; p <= right; p++) {
+			T tmp = l.get(p);
+			for (j = p; j > left && tmp.compareTo(l.get(j - 1)) < 0; j--) {
+				l.set(j, l.get(j - 1));
+			}
+			l.set(j, tmp);
+		}
 	}
 
+	private void quicksort(List<T> l, int left, int right) {
+		if (left + CUTOFF <= right) {
+			T pivot = random(l, left, right);
 
+			int i = left-1;
+			int j = right;
 
+			for (;;) {
+				while (l.get(++i).compareTo(pivot) < 0) {
+				}
+				while (l.get(--j).compareTo(pivot) > 0) {
+				}
+				if (i < j)
+					swap(l, i, j);
+				else
+					break;
+			}
 
+			swap(l, i, right); // restore pivot
+
+			quicksort(l, left, i - 1); // sort small elements
+			quicksort(l, i + 1, right); // sort large elements
+//			System.out.println("\n");
+//			System.out.println("Detta Ã¤r pivot: "+ pivot);
+//			for(int e = 0 ; e<=right; e++)
+//				System.out.print(l.get(e)+" ");
+		} else 
+		{
+			insertionSort(l, left, right);
+		}
+	}
+
+	private T random(List<T> l, int left, int right) 
+	{
+		Random rnd = new Random();
+		int pivoNr = rnd.nextInt(right-left)+left-1;
+		System.out.print(pivoNr+ " ");
+
+		swap(l, pivoNr, right);
+		return l.get(right);
+	}
+
+	@Override
+	protected void doSort(List<T> l) 
+	{
+		quicksort(l, 0, l.size() - 1);
+	}
+
+	@SuppressWarnings("unchecked")
+	public static void main (String[]args)
+	{
+		List<Integer> myList = new ArrayList<Integer>();
+
+		myList.add(5);
+		myList.add(5);
+		myList.add(5);
+		myList.add(5);
+		myList.add(5);
+		myList.add(5);
+		myList.add(5);
+		myList.add(5);
+		myList.add(5);
+		myList.add(5);
+		myList.add(5);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(32);
+		myList.add(32);
+		myList.add(32);
+		myList.add(11);
+		myList.add(443);
+		myList.add(546);
+		myList.add(325);
+		myList.add(116);
+		myList.add(2);
+		myList.add(54);
+		myList.add(89);
+		myList.add(1);
+		myList.add(98);
+		myList.add(246);
+		myList.add(3265);
+		myList.add(234);
+		myList.add(11);
+		myList.add(443);
+		myList.add(546);
+		myList.add(325);
+		myList.add(116);
+		myList.add(2);
+		myList.add(54);
+		myList.add(89);
+		myList.add(1);
+		myList.add(98);
+		myList.add(246);
+		myList.add(3265);
+		myList.add(234);
+		myList.add(11);
+		myList.add(443);
+		myList.add(546);
+		myList.add(325);
+		myList.add(116);
+		myList.add(2);
+		myList.add(54);
+		myList.add(89);
+		myList.add(1);
+		myList.add(98);
+		myList.add(246);
+		myList.add(3265);
+		myList.add(234);
+		myList.add(11);
+		myList.add(443);
+		myList.add(546);
+		myList.add(325);
+		myList.add(116);
+		myList.add(2);
+		myList.add(54);
+		myList.add(89);
+		myList.add(1);
+		myList.add(98);
+		myList.add(246);
+		myList.add(3265);
+		myList.add(234);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(5);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(11);
+		myList.add(443);
+		myList.add(546);
+		myList.add(325);
+		myList.add(116);
+		myList.add(2);
+		myList.add(54);
+		myList.add(89);
+		myList.add(1);
+		myList.add(98);
+		myList.add(246);
+		myList.add(3265);
+		myList.add(234);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(5);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(11);
+		myList.add(443);
+		myList.add(546);
+		myList.add(325);
+		myList.add(116);
+		myList.add(2);
+		myList.add(54);
+		myList.add(89);
+		myList.add(1);
+		myList.add(98);
+		myList.add(246);
+		myList.add(3265);
+		myList.add(234);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(5);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(11);
+		myList.add(443);
+		myList.add(546);
+		myList.add(325);
+		myList.add(116);
+		myList.add(2);
+		myList.add(54);
+		myList.add(89);
+		myList.add(1);
+		myList.add(98);
+		myList.add(246);
+		myList.add(3265);
+		myList.add(234);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(5);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(11);
+		myList.add(443);
+		myList.add(546);
+		myList.add(325);
+		myList.add(116);
+		myList.add(2);
+		myList.add(54);
+		myList.add(89);
+		myList.add(1);
+		myList.add(98);
+		myList.add(246);
+		myList.add(3265);
+		myList.add(234);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(5);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(11);
+		myList.add(443);
+		myList.add(546);
+		myList.add(325);
+		myList.add(116);
+		myList.add(2);
+		myList.add(54);
+		myList.add(89);
+		myList.add(1);
+		myList.add(98);
+		myList.add(246);
+		myList.add(3265);
+		myList.add(234);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(5);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(11);
+		myList.add(443);
+		myList.add(546);
+		myList.add(325);
+		myList.add(116);
+		myList.add(2);
+		myList.add(54);
+		myList.add(89);
+		myList.add(1);
+		myList.add(98);
+		myList.add(246);
+		myList.add(3265);
+		myList.add(234);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(5);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(11);
+		myList.add(443);
+		myList.add(546);
+		myList.add(325);
+		myList.add(116);
+		myList.add(2);
+		myList.add(54);
+		myList.add(89);
+		myList.add(1);
+		myList.add(98);
+		myList.add(246);
+		myList.add(3265);
+		myList.add(234);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(5);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(11);
+		myList.add(443);
+		myList.add(546);
+		myList.add(325);
+		myList.add(116);
+		myList.add(2);
+		myList.add(54);
+		myList.add(89);
+		myList.add(1);
+		myList.add(98);
+		myList.add(246);
+		myList.add(3265);
+		myList.add(234);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(5);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(11);
+		myList.add(443);
+		myList.add(546);
+		myList.add(325);
+		myList.add(116);
+		myList.add(2);
+		myList.add(54);
+		myList.add(89);
+		myList.add(1);
+		myList.add(98);
+		myList.add(246);
+		myList.add(3265);
+		myList.add(234);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(11);
+		myList.add(443);
+		myList.add(546);
+		myList.add(325);
+		myList.add(116);
+		myList.add(2);
+		myList.add(54);
+		myList.add(89);
+		myList.add(1);
+		myList.add(98);
+		myList.add(246);
+		myList.add(3265);
+		myList.add(234);
+		System.out.println(myList);
+
+		QuickSorterRandom rn = new QuickSorterRandom();
+		rn.doSort(myList);
+		System.out.println("\n"+ myList);
+	}
 }
