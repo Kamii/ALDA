0b4e1717930c219f3916efb1428fd9cb2ec442f0
Kamyar Sajjadi <kamyar_sajjadi@gmail.com>
2012-02-07 13:03:51 +0100

    I think this is working now MergeSorter

--- vecka6/MergeSorter.java
+++ vecka6/MergeSorter.java
@@ -1,20 +1,102 @@
-package alda.sort;
+// package alda.sort;
 
-import java.util.List;
+//import java.util.List;
+import java.util.*;
 
 /**
- * Detta ‰r en av algoritmerna du ska implementera sj‰lv. H‰r fÂr du, till
- * skillnad mot de ˆvriga fallen, anv‰nda en extra array.
+ * Detta √§r en av algoritmerna du ska implementera sj√§lv. H√§r f√•r du, till
+ * skillnad mot de √∂vriga fallen, anv√§nda en extra array.
  * 
- * Se kursboken sidan 258 och framÂt. T‰nk dock pÂ att implementationen i boken
- * jobbar pÂ arrayer, men den h‰r jobbar pÂ en lista av godtycklig typ.
+ * Se kursboken sidan 258 och fram√•t. T√§nk dock p√• att implementationen i boken
+ * jobbar p√• arrayer, men den h√§r jobbar p√• en lista av godtycklig typ.
  */
 public class MergeSorter<T extends Comparable<? super T>> extends Sorter<T> {
 
 	@Override
-	protected void doSort(List<T> l) {
-		// TODO Auto-generated method stub
-
+	protected void doSort(List<T> list) {
+		mergeSort(list);
 	}
 
+	/**
+	 * Internal method for having fun
+	 * @param a an array of Comparable items.
+	 */
+	@SuppressWarnings("unckecked")
+	private void mergeSort(List<T> list)
+	{
+		T[] tmpArray = (T[]) new Comparable[list.size()];
+
+		mergeSort(list, tmpArray, 0, list.size() - 1 );
+	}
+
+	/**
+	 * Internal method that makes recursive calls.
+	 * @param a an array of Comparable items.
+	 * @param tmpArray an array to place the merged result.
+	 * @param left the left-most index of the subarray.
+	 * @param right the right-most index of the subarray.
+	 */
+	private void mergeSort(List<T> list, T[] tmpArray, int left, int right )
+	{
+		if(left < right)
+		{
+			int center = ( left + right ) / 2;
+			mergeSort(list, tmpArray, left, center );
+			mergeSort(list, tmpArray, center + 1, right );
+			merge(list, tmpArray, left, center + 1, right );
+		}
+	}
+
+	/**
+	 * Internal method that merges two sorted halves of a subarray.
+	 * @param a an array of Comparable items.
+	 * @param tmpArray an array to place the merged result.
+	 * @param leftPos the left-most index of the subarray.
+	 * @param rightPos the index of the start of the second half.
+	 * @param rightEnd the right-most index of the subarray.
+	 */
+	private void merge(List<T> list, T[] tmpArray, int leftPos, int rightPos, int rightEnd )
+	{
+		int leftEnd = rightPos - 1;
+		int tmpPos = leftPos;
+		int numElements = rightEnd - leftPos + 1;
+
+		// Main loop
+		while(leftPos <= leftEnd && rightPos <= rightEnd)
+			if(list.get(leftPos).compareTo(list.get(rightPos)) <= 0)
+				tmpArray[tmpPos++] = list.get(leftPos++);
+			else
+				tmpArray[tmpPos++] = list.get(rightPos++);
+
+		while(leftPos <= leftEnd)    // Copy rest of first half
+			tmpArray[ tmpPos++ ] = list.get(leftPos++);
+
+		while( rightPos <= rightEnd )  // Copy rest of right half
+			tmpArray[ tmpPos++ ] = list.get(rightPos++);
+
+		// Copy tmpArray back
+		for( int i = 0; i < numElements; i++, rightEnd-- )
+			list.set(rightEnd, tmpArray[rightEnd]);
+	}
+
+	public static void main (String[]args)
+	{
+		List<Integer> myList = new ArrayList<Integer>();
+
+		myList.add(5);
+		myList.add(3);
+		myList.add(7);
+		myList.add(6);
+		myList.add(6);
+		myList.add(43);
+		myList.add(56);
+		myList.add(32);
+		myList.add(11);
+		System.out.println(myList);
+
+		MergeSorter ms = new MergeSorter();
+		ms.doSort(myList);
+		System.out.println(myList);
+
+	}
 }
