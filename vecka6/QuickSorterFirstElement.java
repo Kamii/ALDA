78536ba38d47104a81a6ac32d188516e2ac99407
130n <leon.hennings@gmail.com>
2012-02-09 12:53:06 +0100

    la till QSrandom med iterator, first element utan iterator och first element med iterator

--- vecka6/QuickSorterFirstElement.java
+++ vecka6/QuickSorterFirstElement.java
@@ -1,24 +1,21 @@
-//package alda.sort;
+// package alda.sort;
+
+// import java.util.List;
 import java.util.*;
-import java.util.List;
-import java.util.LinkedList;
-import java.util.ListIterator;
 
 /**
  * Detta är en av algoritmerna du ska implementera själv. Algoritmen ska vara
- * quicksort, och pivotvärdet ska alltid vara det första värdet, det som på
- * sidan 265 i boken förklaras vara "a wrong way".
+ * quicksort, och pivotvärdet ska väljas slumpmässigt, det som på
+ * sidan 266 i boken förklaras vara "a safe maneuver".
  * 
  * För information om quicksort generellt: se kursboken sidan 264 och framåt.
  * Tänk dock på att implementationen i boken jobbar på arrayer, men den här
  * jobbar på en lista av godtycklig typ och att den använder en annan
  * pivotvalsstrategi vilket kräver att man skriver om den lite grann.
  */
-public class QuickSorterFirstElement<T extends Comparable<? super T>> extends
-		Sorter<T> {
+public class QuickSorterFirstElement<T extends Comparable<? super T>> extends Sorter<T> {
 
 	private static final int CUTOFF = 10;
-//inte ändrad
 
 	private void insertionSort(List<T> l, int left, int right) {
 		int j;
@@ -30,74 +27,72 @@ public class QuickSorterFirstElement<T extends Comparable<? super T>> extends
 			l.set(j, tmp);
 		}
 	}
-/*
-	private void insertionSort(List<T> l, int left, int right) {
-		int j;
-        ListIterator<T> insertIter = l.listIterator(left+1);
-        //stega fram till startpunkt
-        //for(int i = 0 ; i < left + 1 ; i++){insertIter.next();}
-//yttre loop
-		for (int p = left + 1; p <= right; p++) {
-			T tmp = insertIter.next();
-            ListIterator<T> innerIter = insertIter;
-//inre loop
-            T innerTmp = innerIter.previous();
-			for (j = p; j > left && tmp.compareTo(innerTmp=innerIter.previous() ) < 0; j--) {
-				l.set(j, innerTmp);
-			}
-			l.set(j, tmp);
-		}
-	}
-*/
+
 	private void quicksort(List<T> l, int left, int right) {
 		if (left + CUTOFF <= right) {
 			T pivot = firstElement(l, left, right);
 
 			int i = left-1;
 			int j = right+1;
-            ListIterator<T> leftIter = l.listIterator(left);
-            ListIterator<T> rightIter = l.listIterator(right);
+
 			for (;;) {
-                do{i=leftIter.nextIndex();
-                }
-				while (leftIter.hasNext() && (leftIter.next().compareTo(pivot) < 0));
-                do{j=rightIter.previousIndex();
-                }
-				while (rightIter.hasPrevious() && (rightIter.previous().compareTo(pivot) > 0));
-				
-				if (i < j){
+				while (l.get(++i).compareTo(pivot) < 0) {
+				}
+				while (l.get(--j).compareTo(pivot) > 0) {
+				}
+				if (i < j)
 					swap(l, i, j);
-                }
 				else
 					break;
 			}
 
 			swap(l, i, right); // restore pivot
+//			System.out.println(l);
 
-			quicksort(l, left, i - 1); // sort small elements
-			quicksort(l, i + 1, right); // sort large elements
-		} else {
+			// Vänstra och högra ska kollas
+			quicksort(l, left, i-1); // sort small elements
+			quicksort(l, i, right); // sort large elements
+		} else 
+		{
 			insertionSort(l, left, right);
 		}
 	}
+
 	private T firstElement(List<T> l, int left, int right) {
         T first = l.get(left);
         swap(l, left, right);
         return first;  
     }
 	@Override
-	protected void doSort(List<T> l) {
+	protected void doSort(List<T> l) 
+	{
 		quicksort(l, 0, l.size() - 1);
 	}
-    public static void main(String[] args){
-        List<Integer> a = new LinkedList<Integer>();
-        Random r = new Random();
-        for(int i = 0; i<10000; i++){
-            a.add(r.nextInt(10000));
-        }
-        QuickSorterFirstElement qs = new QuickSorterFirstElement();
-        qs.doSort(a);
-        System.out.println(a);
-    }
 
+//	private static void copy(List l, int index1, int index2) {
+//		l.set(index1, l.get(index2));
+//	}
+
+	@SuppressWarnings("unchecked")
+	public static void main (String[]args)
+	{
+		List<Integer> myList = new ArrayList<Integer>();
+
+		Random r = new Random();
+		int maxNoDuplicates = (myList.size() / 100); 
+		int count = 0;
+
+		for(int e = 0; e<21; e++)
+			myList.add(r.nextInt(20000));
+
+//		do {
+//			count++;
+//			copy(myList, r.nextInt(myList.size()), r.nextInt(myList.size()));
+//		} while (count < maxNoDuplicates);
+//		System.out.println(myList);
+
+		QuickSorterFirstElement rn = new QuickSorterFirstElement();
+		rn.doSort(myList);
+		System.out.println(myList);
+	}
 }
