d30fb1ddddb8f48394dd18e3e8e0af0062ad0ed2
130n <leon.hennings@gmail.com>
2012-02-21 13:47:24 +0100

    intärn point klass gjord i ClosestPair

--- vecka8/ClosestPair.java
+++ vecka8/ClosestPair.java
@@ -1,9 +1,27 @@
 public class ClosestPair{
-private 
+    private Set<Point> plane = new HashSet<Point>();
 
+    private class Point implements Comparable<Point>{
+        private int x;
+        private int y;
+        public Point(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+        public int getX(){
+            return x;
+        }
+        public int getY(){
+            return y;
+        }
+        public int compareTo(Point p){
+            return x - p.getX();
+        }
+    }
+    public void addPoint(Point p)
 
-public void findClosestPair{
+    public void findClosestPair{
 
-}
+    }
 
 }
--- vecka8/ClosestPairTester.java
+++ vecka8/ClosestPairTester.java
@@ -1,4 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.*;
 public class ClosestPairTester{
 
+	@Before
+	public void setUp() {
 
+    }
+
+	@Test
+	public void testFindClosestPair() {
+
+    }
 }
