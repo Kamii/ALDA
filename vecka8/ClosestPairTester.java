e93cbe25be51ba6b8d5dcc530e9a0f6afd9589d4
Kamyar Sajjadi <kamyar_sajjadi@gmail.com>
2012-02-22 19:44:00 +0100

    Test added

--- vecka8/ClosestPairTester.java
+++ vecka8/ClosestPairTester.java
@@ -1,25 +1,99 @@
 import static org.junit.Assert.*;
+import java.awt.Point;
+import java.util.*;
 
 import org.junit.*;
 public class ClosestPairTester{
-    private ClosestPair cp = new ClosestPair();
 	@Before
-	public void setUp() {
+		public void setUp() 
+		{
 
-    }
+		}
 
 	@Test
-	public void testFindClosestPair() {
+		public void testFindClosestPair() 
+		{
+			ClosestPair cp = new ClosestPair();
+			cp.addPoint(new Point(2,6));
+			cp.addPoint(new Point(3,7));
+			cp.addPoint(new Point(4,5));
+			cp.addPoint(new Point(4,3));
+			cp.addPoint(new Point(5,6));
+			cp.addPoint(new Point(7,6));
+			cp.addPoint(new Point(8,3));
+			cp.addPoint(new Point(9,6));
+			cp.addPoint(new Point(11,8));
+			cp.addPoint(new Point(12,3));
+			assertEquals(cp.findClosestInInterval(0,cp.getPlaneSize()-1), cp.findClosestPair(), 0.00);
+		}
 
-    }
-    
-    /**
-    *   Testfall: (1,10) (100,10) (90, 300), (15, 700) , (55, 2000), (42, 9000)
-    *   två koordinater som är långt ifrån varandra på x-axeln men ändå är närmast
-    *
-    */
+	/**
+	 *   Testfall: (1,10) (100,10) (90, 300), (15, 700) , (55, 2000), (42, 9000)
+	 *   två koordinater som är långt ifrån varandra på x-axeln men ändå är närmast
+	 *
+	 */
 	@Test
-	public void testClosestPairFirstAndLast() {
+		public void testClosestPairFirstAndLast() 
+		{
+			ClosestPair cp = new ClosestPair();
+			cp.addPoint(new Point(12,40));
+			cp.addPoint(new Point(1,4));
+			cp.addPoint(new Point(52,7));
+			cp.addPoint(new Point(245,6754));
+			cp.addPoint(new Point(12245,25));
+			cp.addPoint(new Point(114,432));
+			cp.addPoint(new Point(746,2));
+			cp.addPoint(new Point(452,54));
+			cp.addPoint(new Point(1325,625));
+			cp.addPoint(new Point(7876,96));
+			cp.addPoint(new Point(12,22));
+			cp.addPoint(new Point(245,765));
+			cp.addPoint(new Point(13,657));
+			cp.addPoint(new Point(345,63));
+			assertEquals(cp.findClosestInInterval(0,cp.getPlaneSize()-1), cp.findClosestPair(), 0.00);
+		}
+	
+	@Test
+		public void randomTest1000()
+		{
+			ClosestPair cp = new ClosestPair();
+			Random r = new Random();
 
-    }
+			for(int i = 0; i<100; i++)
+				cp.addPoint(new Point(r.nextInt(1000), r.nextInt(1000)));
+			assertEquals(cp.findClosestInInterval(0,cp.getPlaneSize()-1), cp.findClosestPair(), 0.00);
+		}
+
+	@Test
+		public void randomTestr5000()
+		{
+			ClosestPair cp = new ClosestPair();
+			Random r = new Random();
+
+			for(int i = 0; i<5000; i++)
+				cp.addPoint(new Point(r.nextInt(5000), r.nextInt(5000)));
+			assertEquals(cp.findClosestInInterval(0,cp.getPlaneSize()-1), cp.findClosestPair(), 0.00);
+		}
+
+	@Test
+		public void randomTestr10000()
+		{
+			ClosestPair cp = new ClosestPair();
+			Random r = new Random();
+
+			for(int i = 0; i<10000; i++)
+				cp.addPoint(new Point(r.nextInt(10000), r.nextInt(10000)));
+			assertEquals(cp.findClosestInInterval(0,cp.getPlaneSize()-1), cp.findClosestPair(), 0.00);
+		}
+
+	@Test
+		public void randomTestr20000()
+		{
+			ClosestPair cp = new ClosestPair();
+			Random r = new Random();
+
+			for(int i = 0; i<20000; i++)
+				cp.addPoint(new Point(r.nextInt(100000), r.nextInt(100000)));
+			assertEquals(cp.findClosestInInterval(0,cp.getPlaneSize()-1), cp.findClosestPair(), 0.00);
+		}
 }
